7b16e53157c68d0eefebd5bf88da11de27b829e6
Timofey Serov <115070403+Dedule@users.noreply.github.com>
2023-04-04 15:57:20 +0300

    url fix

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private static final String URL = "jdbc:mysql://localhost:3306";
+    private static final String URL = "jdbc:mysql://localhost:3306/my_database";
     private static final String LOG = "root";
     private static final String PASS = "0000";
 
